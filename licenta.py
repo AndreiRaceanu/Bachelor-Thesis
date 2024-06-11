73b1ef2af58088796afd8d01b0956fb8c7a2bfaf
AndreiRaceanu <andreiraceanu01@gmail.com>
2024-06-11 21:38:22 +0300

    Un update inainte de reboot

--- licenta.py
+++ licenta.py
@@ -6,6 +6,31 @@ def read_data (file_path):
         matrix.append( list( map(lambda x : float(x) , tokens) ) )
     return matrix 
 
+
+N = 2    # Number of files used for training
+X = []   # Training data_set
+for it in range(1,N+1): 
+    X.append(read_data(f"training_{it}.csv"))
+
+
+
+
+
+#print(X[0][0][0])
+#print(X[1][0][0])
 # print(read_data("training_1.csv")[0])
 
 
+"""
+PSEUDO_COD
+
+    poly_lib = ps.PolynomialLibrary(degree=int(param1))
+    trig_lib = ps.FourierLibrary(n_frequencies=int(param2))
+    custom_lib = poly_lib + trig_lib
+    feature_library = custom_lib
+model = ps.SINDy(
+    differentiation_method=smooth_finite_difference,
+    feature_library=feature_library,
+    optimizer=optimizer,
+    feature_names=["x", "y"],
+"""
