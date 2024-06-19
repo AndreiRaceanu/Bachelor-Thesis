8bb13bb05f84842a67555bf28eb41ca7c26f095a
AndreiRaceanu <andreiraceanu01@gmail.com>
2024-06-19 18:29:40 +0300

    mesah

--- licenta.py
+++ licenta.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import time
 
+import matplotlib
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -19,8 +20,6 @@ F5|FC1|P5|CP1|P4|PO8|FP2|FC6|FZ|PZ
 
 eeg_channels = ["CH_F5", "CH_FC1", "CH_P5", "CH_CP1", "CH_P4", "CH_PO8", "CH_FP2", "CH_FC6", "CH_FZ", "CH_PZ"]
 
-file1 = 'training_1.csv'
-file2 = 'training_2.csv'
 DATA_WIDTH = 11  # number of columns used from the csv file
 
 ALPHA = 1
@@ -31,6 +30,8 @@ alpha_bounds = ('cont', [0, 1e-12])
 
 GPGO_ITERATIONS = 20
 CPU_CORES_FOR_GPGO = int(os.getenv('CPU_CORES_FOR_GPGO', 4))
+
+SHOW_PLOTS = False  # whether to show the plots interactively or not (recommend to use False when displaying many plots)
 
 # Initialize history storage
 hyperparameter_history = []
@@ -86,27 +87,29 @@ def get_fitted_model(x, degree, lambda_val, n_frequencies, threshold):
     return model.fit(x, t=t)
 
 
-def plot_derivatives(file_name, actual_derivative, expected_derivative):
+def plot_derivatives(file_name, computed_derivative, predicted):
+    global plot_derivatives_runs
+    runs = plot_derivatives_runs
     # plot the 2 derivatives, fully
-    for derivative_type, derivative in {'actual': actual_derivative, 'expected': expected_derivative}.items():
+    for derivative_type, derivative in {'predicted': predicted, 'computed': computed_derivative}.items():
         plt.figure()
         plt.plot(t, derivative)
         plt.xlabel('Time')
         plt.ylabel('Derivative')
         plt.title(f'{derivative_type} Derivative for {file_name}')
-        plt.savefig(f'out/derivative_{file_name}_full_{derivative_type}.png', bbox_inches='tight')
-        plt.show()
+        plt.savefig(f'out/derivative{runs}_{file_name}_full_{derivative_type}.png', bbox_inches='tight')
+        plt.show() if SHOW_PLOTS else plt.close()
     # plot a plot for each set of channels from both derivatives
     for i, channel in enumerate(eeg_channels):
         plt.figure(figsize=(12, len(eeg_channels)))
-        plt.plot(t_columns, actual_derivative[i], label=f'{channel} actual derivative')
-        plt.plot(t_columns, expected_derivative[i], label=f'{channel} expected derivative')
+        plt.plot(t_columns, predicted[i], label=f'{channel} predicted derivative')
+        plt.plot(t_columns, computed_derivative[i], label=f'{channel} computed derivative')
         plt.xlabel('Time (s)')
         plt.ylabel('Amplitude')
-        plt.title(f'actual vs expected for data {file_name} for channel {channel}')
+        plt.title(f'predicted vs computed for data {file_name} for channel {channel}')
         plt.legend(loc='upper right')
-        plt.savefig(f'out/derivative_{file_name}_{channel}.png', bbox_inches='tight')
-        plt.show()
+        plt.savefig(f'out/derivative{runs}_{file_name}_{channel}.png', bbox_inches='tight')
+        plt.show() if SHOW_PLOTS else plt.close()
 
 
 def plot_hyperparams_and_error():
@@ -124,7 +127,7 @@ def plot_hyperparams_and_error():
     plt.legend()
     plt.title('Evolution of Hyperparameters (degree & n_frequencies)')
     plt.savefig(f'{savefig_prefix}_degree_and_n_frequencies.png', bbox_inches='tight')
-    plt.show()
+    plt.show() if SHOW_PLOTS else plt.close()
     # Plot evolution of hyperparameters (lambda_val)
     plt.figure()
     plt.plot(hyperparameter_history_as_np_array[:, 2], label='lambda_val')
@@ -133,7 +136,7 @@ def plot_hyperparams_and_error():
     plt.legend()
     plt.title('Evolution of Hyperparameters (lambda_val)')
     plt.savefig(f'{savefig_prefix}_lambda_val.png', bbox_inches='tight')
-    plt.show()
+    plt.show() if SHOW_PLOTS else plt.close()
     # Plot evolution of hyperparameters (threshold)
     plt.figure()
     plt.plot(hyperparameter_history_as_np_array[:, 3], label='threshold')
@@ -142,7 +145,7 @@ def plot_hyperparams_and_error():
     plt.legend()
     plt.title('Evolution of Hyperparameters (threshold)')
     plt.savefig(f'{savefig_prefix}_threshold.png', bbox_inches='tight')
-    plt.show()
+    plt.show() if SHOW_PLOTS else plt.close()
     # Plot evolution of error
     plt.figure()
     plt.plot(sorted(error_history, reverse=True), label='Error')
@@ -151,7 +154,7 @@ def plot_hyperparams_and_error():
     plt.legend()
     plt.title('Evolution of Error')
     plt.savefig(f'out/error{plot_hyperparams_and_error_runs}_threshold.png', bbox_inches='tight')
-    plt.show()
+    plt.show() if SHOW_PLOTS else plt.close()
 
 
 def plot_data():
@@ -165,7 +168,7 @@ def plot_data():
         plt.title(f'EEG Data from {name}')
         plt.legend(loc='upper right')
         plt.savefig(f'out/data_{name}.png', bbox_inches='tight')
-        plt.show()
+        plt.show() if SHOW_PLOTS else plt.close()
 
 
 def run_gpgo_and_get_results(data):
@@ -189,20 +192,25 @@ def run_gpgo_and_get_results(data):
 
 
 plot_hyperparams_and_error_runs = 0
+plot_derivatives_runs = 0
 
 # Load data
-file_names = ['training_1.csv', 'training_2.csv', 'validation_1.csv']
+
+file1 = 'training_1.csv'
+file2 = 'training_2.csv'
+file3 = 'validation_1.csv'
+file_names = [file1, file2, file3]
 data_dict = {file_name: read_data(file_name, DATA_WIDTH) for file_name in file_names}
-data1 = data_dict['training_1.csv']
-data2 = data_dict['training_2.csv']
-data3 = data_dict['validation_1.csv']
+data1 = data_dict[file1]
+data2 = data_dict[file2]
+data3 = data_dict[file3]
 
 # Assuming time vector t and derivative x_dot are known
 # For the sake of this example, let's create synthetic ones
 t = np.linspace(1, len(data1), len(data1), dtype=int)
 t_columns = np.linspace(1, DATA_WIDTH, DATA_WIDTH, dtype=int)
 # x = data1
-# x_dot = np.gradient(data1, axis=0)  # Replace this with actual derivative if available
+# x_dot = np.gradient(data1, axis=0)  # Replace this with computed derivative if available
 
 # Define the parameter space
 # TODO: find the type for lambda_val & threshold
@@ -245,12 +253,67 @@ if 429 Too Many Requests is thrown by PyCharm when plotting the graphs, then
 https://youtrack.jetbrains.com/issue/PY-43687/Problems-with-many-plots-in-scientific-view#focus=Comments-27-6266042.0-0
 """
 
+# make sure the out dir exists
+if not os.path.exists('out'):
+    os.makedirs('out')
+
+# to avoid Tcl_AsyncDelete: async handler deleted by the wrong thread
+# (see more at https://github.com/matplotlib/matplotlib/issues/27713)
+if not SHOW_PLOTS:
+    matplotlib.use('Agg')
+
 plot_data()
 plot_hyperparams_and_error()
-plot_derivatives('training_1.csv', data1_x_dot, data1_x_dot_predicted)
+plot_derivatives(file1, data1_x_dot, data1_x_dot_predicted)
 data2_error, model2, data2_x_dot, data2_x_dot_predicted = get_error_and_derivatives(model1, data2, degree_best,
                                                                                     lambda_best,
                                                                                     n_frequencies_best,
                                                                                     threshold_best)
-plot_derivatives('training_2.csv', data2_x_dot, data2_x_dot_predicted)
 plot_hyperparams_and_error()
+plot_derivatives(file2, data2_x_dot, data2_x_dot_predicted)
+print("plotted graphs after 1st GPGO run")
+
+best_params2 = run_gpgo_and_get_results(data2)
+
+best_error1 = best_params1[1]
+best_error2 = best_params2[1]
+best_error = None
+
+if best_error1 < best_error2:
+    best_params = best_params1
+    print("Best parameters are from the 1st run")
+else:
+    best_params = best_params2
+    print("Best parameters are from the 2nd run")
+
+degree_best = int(best_params[0]['degree'])
+n_frequencies_best = int(best_params[0]['n_frequencies'])
+lambda_best = best_params[0]['lambda_val']
+threshold_best = best_params[0]['threshold']
+
+data1_error, model1, data1_x_dot, data1_x_dot_predicted = get_error_model_and_derivatives(data1, degree_best,
+                                                                                          lambda_best,
+                                                                                          n_frequencies_best,
+                                                                                          threshold_best,
+                                                                                          save_metadata=False)
+plot_derivatives_runs += 1
+plot_hyperparams_and_error()
+plot_derivatives(file1, data1_x_dot, data1_x_dot_predicted)
+
+data2_error, model2, data2_x_dot, data2_x_dot_predicted = get_error_model_and_derivatives(data2, degree_best,
+                                                                                          lambda_best,
+                                                                                          n_frequencies_best,
+                                                                                          threshold_best,
+                                                                                          save_metadata=False)
+
+plot_derivatives(file2, data2_x_dot, data2_x_dot_predicted)
+print("plotted graphs after 2nd GPGO run")
+
+data3_error, model3, data3_x_dot, data3_x_dot_predicted = get_error_model_and_derivatives(data3, degree_best,
+                                                                                          lambda_best,
+                                                                                          n_frequencies_best,
+                                                                                          threshold_best)
+
+plot_hyperparams_and_error()
+plot_derivatives(file3, data3_x_dot, data3_x_dot_predicted)
+print()
